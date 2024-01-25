87f0dc6ee3d9a5b2a7343b5c1595ea6d4d78eb2f
bychenko-dmitriy <bychenko.dmitriy@vrn.moy-ka.ru>
2024-01-25 16:45:57 +0300

    fix env

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: configService.get<string>('databaseUrl'),
-      },
-    },
-  });
   await app.listen(3000);
 }
 bootstrap();
