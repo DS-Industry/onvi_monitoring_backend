d24f506f9ba91159c0de915bbf23a5520ee94594
anjali-0987 <a.sharm@moy-ka.com>
2024-07-18 17:47:45 +0530

    Feat(usercasl)

--- src/app/platform-user/object/object.module.ts
+++ src/app/platform-user/object/object.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@db/prisma/prisma.module';
 import { ObjectPermissionsRepositoryProvider } from '@platform-user/object/provider/object';
-import { GetByIdObjectUseCase } from './user-case/object-get-by-id';
+import { GetByIdObjectUseCase } from '@platform-user/object/use-case/object-get-by-id';
 
 @Module({
   imports: [PrismaModule],
