4f81c99ea471ded82d00249c8c4e4e1ff04dd5ff
bychenko-dmitriy <bychenko.dmitriy@vrn.moy-ka.ru>
2024-10-17 14:36:33 +0300

    fix handler data raw

--- src/app/platform-device/device-data-raw/controller/device-data-raw.ts
+++ src/app/platform-device/device-data-raw/controller/device-data-raw.ts
@@ -1,25 +1,22 @@
-import {
-  Controller,
-  HttpCode,
-  Post,
-  Request,
-} from '@nestjs/common';
+import { Controller, HttpCode, Post, Request, UseGuards } from '@nestjs/common';
 import { CreateDeviceDataRawUseCase } from '@pos/device/device-data/device-data-raw/use-cases/device-data-raw-create';
 import { ApiKeyAuthGuard } from '@platform-device/device-data-raw/guards/api-key.guards';
 
-@Controller('data/raw')
+@Controller('data')
 export class DeviceDataRawController {
   constructor(
     private readonly deviceDataRawCreate: CreateDeviceDataRawUseCase,
   ) {}
 
-  @Post('')
-  //@UseGuards(ApiKeyAuthGuard)
-  @HttpCode(201)
-  async create(@Request() req: any): Promise<void> {
+  @Post('raw')
+  @UseGuards(ApiKeyAuthGuard)
+  @HttpCode(200)
+  async create(@Request() req: any): Promise<any> {
     try {
-      const data = req.headers.data;
+      const dataReq = req.headers.data;
+      const data = dataReq.substring(1, dataReq.length - 1);
       await this.deviceDataRawCreate.execute(data);
+      return { status: 200 };
     } catch (e) {
       throw new Error(e);
     }
