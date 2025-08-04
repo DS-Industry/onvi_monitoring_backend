443f70d43c1868ec4a6af56662e452c29cef752b
bychenko-dmitriy <bychenko.dmitriy@vrn.moy-ka.ru>
2025-08-04 10:39:35 +0300

    actual version

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -6,10 +6,17 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'],
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
   async onModuleDestroy() {
+    console.log('start prisma connection destr');
     await this.$disconnect();
+    console.log('end prisma connection destr');
   }
 }
