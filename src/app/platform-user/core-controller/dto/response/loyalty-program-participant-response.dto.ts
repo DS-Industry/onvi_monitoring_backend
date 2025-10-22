53387bd6feb9577ecbeab8811b7477019dc32dbd
Nikita Savitskiy <s6475515243@gmail.com>
2025-10-22 17:41:40 +0400

    added description field to get lp response

--- src/app/platform-user/core-controller/dto/response/loyalty-program-participant-response.dto.ts
+++ src/app/platform-user/core-controller/dto/response/loyalty-program-participant-response.dto.ts
@@ -14,6 +14,7 @@ export class LoyaltyProgramParticipantResponseDto {
         ownerOrganizationId: number;
         connectedPoses: number;
         engagedClients: number;
+        description?: string;
     }
 }
 
@@ -37,6 +38,7 @@ export function mapLoyaltyProgramToParticipantResponse(
         ownerOrganizationId: program.ownerOrganizationId,
         connectedPoses: connectedPoses ?? 0,
         engagedClients: engagedClients ?? 0,
+        description: program.description,
     }
   };
 }
